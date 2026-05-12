1193ecf4aa5bb2de622bec8ab47a38b4844bdd6a
Edgar Peredo Gonzales <edgpgj@gmail.com>
2026-05-12 00:54:12 -0400

    feat: implementado SP usp_InsertOrder con manejo de transacciones

---
+++ NorthwindBI/NorthwindOLTP/Schema/Programmability/Stored Procedures/usp_InsertOrder.sql
@@ -0,0 +1,33 @@
+﻿CREATE PROCEDURE [dbo].[usp_InsertOrder]
+    @CustomerID NCHAR(5),
+    @EmployeeID INT,
+    @ShipVia INT,
+    @ProductID INT,
+    @Quantity SMALLINT,
+    @UnitPrice MONEY
+AS
+BEGIN
+    SET NOCOUNT ON;
+    
+    BEGIN TRY
+        BEGIN TRANSACTION;
+
+        -- 1. Insertar el encabezado del pedido
+        DECLARE @NewOrderID INT;
+        
+        INSERT INTO [dbo].[Orders] (CustomerID, EmployeeID, OrderDate, ShipVia)
+        VALUES (@CustomerID, @EmployeeID, GETDATE(), @ShipVia);
+        
+        SET @NewOrderID = SCOPE_IDENTITY();
+
+        -- 2. Insertar el detalle del pedido
+        INSERT INTO [dbo].[OrderDetails] (OrderID, ProductID, UnitPrice, Quantity, Discount)
+        VALUES (@NewOrderID, @ProductID, @UnitPrice, @Quantity, 0);
+
+        COMMIT TRANSACTION;
+    END TRY
+    BEGIN CATCH
+        IF @@TRANCOUNT > 0 ROLLBACK TRANSACTION;
+        THROW;
+    END CATCH
+END
